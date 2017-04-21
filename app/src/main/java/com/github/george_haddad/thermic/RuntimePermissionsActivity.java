67a066960425fb2a0e25f20fc05252a352584a3d
George El-Haddad <george.dma@gmail.com>
2017-04-21 16:28:57 +0300

    Initial commit, working flir app view on the CAT S60 phone

---
+++ app/src/main/java/com/github/george_haddad/thermic/RuntimePermissionsActivity.java
@@ -0,0 +1,90 @@
+package com.github.george_haddad.thermic;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.SparseIntArray;
+import android.view.View;
+
+/**
+ * This class was derived mostly form Truiton
+ * https://github.com/Truiton/RuntimePermissions
+ */
+public abstract class RuntimePermissionsActivity extends Activity {
+    private SparseIntArray mErrorString;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mErrorString = new SparseIntArray();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        int permissionCheck = PackageManager.PERMISSION_GRANTED;
+        for (int permission : grantResults) {
+            permissionCheck = permissionCheck + permission;
+        }
+
+        if ((grantResults.length > 0) && permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            onPermissionsGranted(requestCode);
+        }
+        else {
+            Snackbar.make(findViewById(android.R.id.content), mErrorString.get(requestCode),
+                    Snackbar.LENGTH_INDEFINITE).setAction("ENABLE",
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent();
+                            intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                            intent.addCategory(Intent.CATEGORY_DEFAULT);
+                            intent.setData(Uri.parse("package:" + getPackageName()));
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                            startActivity(intent);
+                        }
+                    }).show();
+        }
+    }
+
+    public void requestAppPermissions(final int stringId, final int requestCode, final String ... requestedPermissions) {
+        mErrorString.put(requestCode, stringId);
+        int permissionCheck = PackageManager.PERMISSION_GRANTED;
+        boolean shouldShowRequestPermissionRationale = false;
+        for (String permission : requestedPermissions) {
+            permissionCheck = permissionCheck + ContextCompat.checkSelfPermission(this, permission);
+            shouldShowRequestPermissionRationale = shouldShowRequestPermissionRationale || ActivityCompat.shouldShowRequestPermissionRationale(this, permission);
+        }
+
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            if (shouldShowRequestPermissionRationale) {
+                Snackbar.make(findViewById(android.R.id.content), stringId,
+                        Snackbar.LENGTH_INDEFINITE).setAction("GRANT",
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                ActivityCompat.requestPermissions(RuntimePermissionsActivity.this, requestedPermissions, requestCode);
+                            }
+                        }).show();
+            }
+            else {
+                ActivityCompat.requestPermissions(this, requestedPermissions, requestCode);
+            }
+        }
+        else {
+            onPermissionsGranted(requestCode);
+        }
+    }
+
+    public abstract void onPermissionsGranted(int requestCode);
+}
